b8e66d5df7841f421cfa53d4ed0fc039a3da1bed
gheougje <rou.leon@icloud.com>
2023-06-20 20:08:10 +0200

    完成了所需要的定理环境, 并通过CSS实现了自动计数. 还需要实现交叉引用

--- includes/TagHooks.php
+++ includes/TagHooks.php
@@ -9,14 +9,15 @@ class tagHooks{
         //$parser->setHook( 'math', [ self::class, 'mathRender']);
         //$parser->setHook( 'tikzcd', [ self::class, 'tikzcdRender']);
 
-        // $parser->setHook( 'definition', [self::class, 'definitionRender']);
-        // $parser->setHook( 'theorem', [self::class, 'theoremRender']);
-        // $parser->setHook( 'proposition', [self::class, 'propositionRender']);
-        // $parser->setHook( 'corollary', [self::class, 'corollaryRender']);
-        // $parser->setHook( 'lemma', [self::class, 'lemmaRender']);
-        // $parser->setHook( 'remark', [self::class, 'remarkRender']);
-        // $parser->setHook( 'example', [self::class, 'exampleRender']);
+        $parser->setHook( 'definition', [self::class, 'definitionRender']);
+        $parser->setHook( 'theorem', [self::class, 'theoremRender']);
+        $parser->setHook( 'proposition', [self::class, 'propositionRender']);
+        $parser->setHook( 'corollary', [self::class, 'corollaryRender']);
+        $parser->setHook( 'lemma', [self::class, 'lemmaRender']);
+        $parser->setHook( 'remark', [self::class, 'remarkRender']);
+        $parser->setHook( 'example', [self::class, 'exampleRender']);
         // $parser->setHook( 'enumerate', [self::class, 'enumerateRender']);
+        // $parser->setHook( 'cref', [self::class, 'crefRender']);
 
         $parser->setHook( 'proofc', [self::class, 'proofcRender']);
     }
@@ -39,8 +40,61 @@ class tagHooks{
 
         $content_dom = new DOMDocument();
         $content_dom->loadHTML(mb_convert_encoding($parser->recursiveTagParse($input, $frame), 'HTML-ENTITIES', 'UTF-8'));
+        
         $input_content = $dom->importNode($content_dom->documentElement, true);
         $content->appendChild($input_content);
+        return $dom->saveHTML();
+    }
+
+    public static function definitionRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'definition');
+    }
+
+    public static function theoremRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'theorem');
+    }
+
+    public static function propositionRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'proposition');
+    }
+
+    public static function corollaryRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'corollary');
+    }
+
+    public static function lemmaRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'lemma');
+    }
+
+    public static function remarkRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'remark');
+    }
+
+    public static function exampleRender( $input, array $args, Parser $parser, PPFrame $frame)
+    {
+        return self::commonEnvironment($input, $args, $parser, $frame, 'example');
+    }
+
+    private static function commonEnvironment( $input, array $args, Parser $parser, PPFrame $frame, string $className)
+    {
+        $dom = new DOMDocument();
+        $blockquote = $dom->createElement('blockquote');
+        $dom->appendChild($blockquote);
+
+        $blockquote->setAttribute('class', $className);
+        if($args['id'] != null ) $blockquote->setAttribute('id', $args['id']);
+
+        $content_dom = new DOMDocument();
+        $content_dom->loadHTML(mb_convert_encoding($parser->recursiveTagParse($input, $frame), 'HTML-ENTITIES', 'UTF-8'));
+
+        $input_content = $dom->importNode($content_dom->documentElement, true);
+        $blockquote->appendChild($input_content);
         return $dom->saveHTML();
     }
 }
